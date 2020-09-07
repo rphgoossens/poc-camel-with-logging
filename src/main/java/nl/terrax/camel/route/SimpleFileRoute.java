e9d5ecf466545e686043640483901c26b55fe16a
rphgoossens <r.p.h.goossens@gmail.com>
2020-09-07 17:39:35 +0200

    Upgraded logging

--- src/main/java/nl/terrax/camel/route/SimpleFileRoute.java
+++ src/main/java/nl/terrax/camel/route/SimpleFileRoute.java
@@ -1,7 +1,10 @@
 package nl.terrax.camel.route;
 
+import nl.terrax.camel.logging.event.StatusEventPublisher;
 import nl.terrax.camel.processor.SimpleProcessor;
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import static org.apache.camel.LoggingLevel.INFO;
@@ -10,9 +13,14 @@ import static org.apache.camel.LoggingLevel.INFO;
 public class SimpleFileRoute extends RouteBuilder {
 
     private final SimpleProcessor simpleProcessor;
+    private final StatusEventPublisher statusEventPublisher;
 
-    public SimpleFileRoute(SimpleProcessor simpleProcessor) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleFileRoute.class);
+
+    public SimpleFileRoute(SimpleProcessor simpleProcessor,
+                           StatusEventPublisher statusEventPublisher) {
         this.simpleProcessor = simpleProcessor;
+        this.statusEventPublisher = statusEventPublisher;
     }
 
     @Override
@@ -20,7 +28,9 @@ public class SimpleFileRoute extends RouteBuilder {
         from("file:/home/rphgoossens/test/logging_in").routeId("camel_logging")
                 .log(INFO, "Picked up file")
                 .process(simpleProcessor)
-                .to("file:/home/rphgoossens/test/logging_out");
+                .to("file:/home/rphgoossens/test/logging_out")
+                .setHeader("CustomLogger", constant(LOGGER))
+                .bean(statusEventPublisher, String.format("create('%s', 'Info', 'Sent file ${header.CamelFileName}')", "SimpleRoute"));
     }
 
 }
